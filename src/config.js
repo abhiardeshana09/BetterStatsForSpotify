24ee80de96de7ac089b7cb568ca3a1a1f5e7c741
Abhi Ardeshana <abhi_ardeshana@yahoo.ca>
2022-09-07 14:31:32 -0400

    added template config.js file

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    clientId: '<Your Spotify Client ID>',
+    clientSecret: '<Your Spotify Client Secret>',
+    redirectUri: '<Your Spotify Redirect URI>'
+}
+
+export default config;
